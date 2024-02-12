4a785fa4d2dd3ba5671e5f58e97e211cbac3ee2e
alfathanam <alfathanam@gmail.com>
2024-02-12 23:29:37 +0700

    Learn about how to install npm and working with NPM. And try to install NPM validator and create gitignore

---
+++ NPM/app.js
@@ -0,0 +1,13 @@
+// if not standard command must npm run dev
+/**
+ *? How to install Npm
+ 1. On terminal, typing npm init for the first time or check node -v
+ 2. If node v showing up then init 
+ 3. Make sure init on folder yang kita buat 
+ 4. Node module automatic created, and for first time no one dependency 
+ 5. Npm i validator or npm i validator@11.0.0 with version --> example to instal npm 
+ 6. Check on package.json, package.json , node-module all changes and thats folder having dependecy
+7. U can uninstall package with typing npm uninstall validator
+ */
+
+console.log("hello world");
