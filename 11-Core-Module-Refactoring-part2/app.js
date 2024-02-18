288c12316ef646d5d038734fe7d2c41340819f71
alfathanam <alfathanam@gmail.com>
2024-02-18 19:29:54 +0700

    Refactor and adjust project contact app with useful terminal, and install npm yargs for easyly for terimnal commands

---
+++ 11-Core-Module-Refactoring-part2/app.js
@@ -0,0 +1,19 @@
+// * Adjust and use another way to create contact app
+// * this is part 2, so let code
+
+console.log(process.argv[2]); //Process is  build existing on node and call without call cause not function
+
+
+
+// const contacts = require("./contacts");
+// // const { tulisPertanyaan, simpanData } = require("./contacts"); // you can do this
+
+// const mainFunc = async () => {
+//   const name = await contacts.tulisPertanyaan("Who is your name?\n");
+//   const email = await contacts.tulisPertanyaan("What is your email?\n");
+//   const noTelp = await contacts.tulisPertanyaan("What is your number phone?\n");
+
+//   contacts.simpanData(name, email, noTelp);
+// };
+
+// mainFunc();
