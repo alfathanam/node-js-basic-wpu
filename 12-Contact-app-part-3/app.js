b47db0b66d5775c5a52b2ef1eb2632417d7add00
alfathanam <alfathanam@gmail.com>
2024-03-11 20:53:56 +0700

    todo add show list name and no hp

--- 12-Contact-app-part-3/app.js
+++ 12-Contact-app-part-3/app.js
@@ -1,39 +1,32 @@
-// * Adjust and use another way to create contact app
-// * this is part 2, so let code
-
-// console.log(process.argv[2]); //Process is  build existing on node and call without call cause not function
 const contacts = require("./contacts");
 const yargs = require("yargs");
-yargs.command({
-  command: "add",
-  describe: "menambahkan contact baru",
-  builder: {
-    name: {
-      describe: "nama lengkap",
-      demandOption: true,
-      type: "string",
+yargs
+  .command({
+    command: "add",
+    describe: "menambahkan contact baru",
+    builder: {
+      name: {
+        describe: "nama lengkap",
+        demandOption: true,
+        type: "string",
+      },
+      email: {
+        describe: "email user",
+        demandOption: false,
+        type: "string",
+      },
+      noHp: {
+        describe: "number phone",
+        demandOption: true,
+        type: "string",
+      },
     },
-    email: {
-      describe: "email user",
-      demandOption: false,
-      type: "string",
+    handler(argv) {
+      contacts.simpanData(argv.name, argv.email, argv.noHp);
     },
-    noHp: {
-      describe: "number phone",
-      demandOption: true,
-      type: "string",
-    },
-  },
-  handler(argv) {
-    // const contact = {
-    //   name: argv.name,
-    //   email: argv.email,
-    //   noHp: argv.noHp,
-    // };
-    // console.log(contact);
+  })
+  .demandCommand();
 
-    contacts.simpanData(argv.name, argv.email, argv.noHp);
-  },
-});
+// Todo Menampilkan daftar semua nama & no Hp
 
 yargs.parse();
