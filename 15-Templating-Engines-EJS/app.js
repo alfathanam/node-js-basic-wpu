f0076033c1abe9e3fac996e0613bde5e6b07f389
alfathanam <alfathanam@gmail.com>
2024-03-12 22:35:44 +0700

    learn how to using EJS template engines, install expresslayout for use main template nad reuse for another file EJS, Learn res.render () and sent data to EJS from res.render

--- 15-Templating-Engines-EJS/app.js
+++ 15-Templating-Engines-EJS/app.js
@@ -1,19 +1,41 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+const expressLayouts = require("express-ejs-layouts");
 //todo Using EJS
 app.set("view engine", "ejs");
-
+app.use(expressLayouts);
 app.get("/", (req, res) => {
-  // res.sendFile("./index.html", { root: __dirname });
-  res.render("index");
+  const mhs = [
+    {
+      nama: "alfathan",
+      email: "alfathan@gmail.com",
+      noHp: "08782372323",
+    },
+    {
+      nama: "aris",
+      email: "aris@gmail.com",
+      noHp: "08782372323",
+    },
+    {
+      nama: "munandar",
+      email: "munandar@gmail.com",
+      noHp: "08782372323",
+    },
+  ];
+  res.render("index", {
+    nama: "Aris",
+    title: "Belajar EJS",
+    mahasiswa: mhs,
+    layout: "layouts/main",
+  });
+  //* sent data nama to index.ejs
 });
 app.get("/contacts", (req, res) => {
-  res.render("contacts");
+  res.render("contacts", { title: "Halaman Contacts", layout: "layouts/main" });
 });
 app.get("/abouts", (req, res) => {
-  res.render("abouts");
+  res.render("abouts", { title: "Halaman Abouts", layout: "layouts/main" });
 });
 
 app.get("/products/:id/category/:idCat", (req, res) => {
