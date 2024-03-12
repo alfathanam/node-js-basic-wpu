b7d48737e12e620c13db3d5d4867c78e488738b1
alfathanam <alfathanam@gmail.com>
2024-03-12 14:24:33 +0700

    create file app js and run npm init -y also install express js version 4.17.1

---
+++ 14-ExpressJS/app.js
@@ -0,0 +1,9 @@
+// * Express JS
+// * Web App Framework, Providing Interface for development web, helping managing data stream
+// * MERN, MEVN, MEAN Mongo,express,react,node
+
+//? Feature on express JS
+//* 1. Handling request dengan method http dengan mudah (routes)
+//* 2. MVC -> model view controller
+//* 3. MiddleWare
+//* 4. terintegrasi dengan view rendering engine, template managing
