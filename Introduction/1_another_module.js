0481b0602851c906ecbdd3c3c792b06a953a35fd
alfathanam <alfathanam@gmail.com>
2024-03-12 14:04:14 +0700

    remove repo introduction

--- Introduction/1_another_module.js
+++
@@ -1,8 +0,0 @@
-let a = 10,
-  b = 20;
-
-const sum = (a, b) => {
-  return a + b;
-};
-
-module.exports = sum(a, b);
--- Introduction/1_modules.js
+++
@@ -1,9 +0,0 @@
-let nama = "alfathan";
-let umur = 31;
-
-// we will export this module, so this module can use on another modules
-const sayHello = (nama, umur) => {
-  return `Nama saya ${nama}, dan umur saya sekarang ${umur} `;
-};
-
-module.exports = sayHello(nama, umur);
