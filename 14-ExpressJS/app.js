cd9057f3ba9d21855c5f16884eb54b6a60e25472
alfathanam <alfathanam@gmail.com>
2024-03-12 15:29:44 +0700

    after we learn using res and function on res, and then learn terkait request seperti request.params, request.query URL

--- 14-ExpressJS/app.js
+++ 14-ExpressJS/app.js
@@ -24,13 +24,22 @@ app.get("/", (req, res) => {
   //     noHp: "08766666121",
   //   });
 
-  res.sendFile("./index.html", { root: __dirname }); // *todo like before, sendFile html
+  res.sendFile("./index.html", { root: __dirname }); // *todo like before, sendFile html,pdf,image,css
 });
 app.get("/contacts", (req, res) => {
   res.sendFile("./contacts.html", { root: __dirname });
 });
 app.get("/abouts", (req, res) => {
   res.sendFile("./abouts.html", { root: __dirname });
+});
+
+//todo Learn about req.params, and req.query
+
+app.get("/products/:id/category/:idCat", (req, res) => {
+  res.send(`this product ${req.params.id} <br> 
+  this category is ${req.params.idCat} <br>
+  this color is ${req.query.color}`); //*id from products/:id
+  //   http://localhost:3000/products/1/category/shoes?color=red
 });
 
 //*  app use
