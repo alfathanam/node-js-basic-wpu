f34fcb0740f7669437ca7fb6c770a8d97c486726
alfathanam <alfathanam@gmail.com>
2024-03-12 15:03:10 +0700

    using template started express js, and adding routes for contacts and abouts and learn about res.send, res.sendFile, res.json() etc, and app.use for middleware this app.use always running, dont place before app.get

--- 14-ExpressJS/app.js
+++ 14-ExpressJS/app.js
@@ -7,3 +7,39 @@
 //* 2. MVC -> model view controller
 //* 3. MiddleWare
 //* 4. terintegrasi dengan view rendering engine, template managing
+
+// Todo do like materi 13 but using express
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  // * Check Documentation res,req , app
+  //   res.send("Hello World!");
+  //   res.sendStatus(200);
+  //   res.json({
+  //     name: "alfathan",
+  //     email: "aris@gmail.com",
+  //     noHp: "08766666121",
+  //   });
+
+  res.sendFile("./index.html", { root: __dirname }); // *todo like before, sendFile html
+});
+app.get("/contacts", (req, res) => {
+  res.sendFile("./contacts.html", { root: __dirname });
+});
+app.get("/abouts", (req, res) => {
+  res.sendFile("./abouts.html", { root: __dirname });
+});
+
+//*  app use
+//* Mounts the specified middleware function or functions at the specified path: the middleware function is executed when the base of the requested path matches path.
+
+app.use("/", (req, res) => {
+  res.send("Default, always dijalankan");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
